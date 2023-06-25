52a8eed21fc42c60c43adea943aafe167926f52a
Justin Pazmino <novalogout@gmail.com>
2023-06-25 01:45:33 -0700

    add example config

---
+++ example_config.py
@@ -0,0 +1,18 @@
+#pymongo
+# MongoDB database
+CONNECT_STRING= ""
+DATABASE_NAME = 'friend_food'
+COLLECTION_NAME = 'costs'
+USER_ID_COLLECTION = 'userid'
+
+#discord
+
+BOT_SECRET = ''
+# Server ID
+GUILD_ID = 0
+
+#Discord ID of the Banker
+BANKER_ID = 0
+
+# Dictionary of Names and Discord ID's
+DISCORD_IDS = {'john doe' : 123}
